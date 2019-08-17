4ca78b4a4cad6fcab14b62d52e05a5179e97857e
Ian <mus5002@aol.com>
2019-08-17 12:51:19 -0400

    created tabs component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(res => {
+        res.data.topics.forEach(topic => {
+            createTab(topic)
+        })
+        return res
+    })
+    .catch(err => {
+        console.log(err)
+        return err
+    })
+
+const createTab = (topic) => {
+    console.log(topic)
+    
+    let tab = document.createElement('div')
+    tab.classList.add('tab')
+    tab.innerText = `${topic}`
+
+    const topicsDiv = document.querySelector('.topics')
+    topicsDiv.appendChild(tab)
+}
